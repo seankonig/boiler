b2b4c425c3dad276eb8cff57c72939ccc4916ecd
Sean Konig <seankonig@Seans-MacBook-Air.local>
2021-09-12 12:55:17 +0200

    fix tests

--- test/commands/create.test.ts
+++ test/commands/create.test.ts
@@ -1,17 +1,25 @@
-import {expect, test} from '@oclif/test'
+import { assert } from 'chai';
+const PromptSelect = require('enquirer/lib/prompts/select');
+let prompt: any;
 
-describe('create', () => {
-  test
-  .stdout()
-  .command(['create'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
+describe('keypress events', () => {
+    it('should handle submitting with <enter>', () => {
+        prompt = new PromptSelect({
+            message: 'prompt-select',
+            choices: [
+                { name: 'a', message: 'A' },
+                { name: 'b', message: 'BB' },
+                { name: 'c', message: 'CCC' },
+                { name: 'd', message: 'DDDD' }
+            ]
+        });
 
-  test
-  .stdout()
-  .command(['create', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
-})
+        prompt.once('run', () => {
+            prompt.keypress(null, { name: 'return' });
+        });
+
+        return prompt.run().then((answer: string) => {
+            assert.equal(answer, 'a');
+        });
+    });
+});
